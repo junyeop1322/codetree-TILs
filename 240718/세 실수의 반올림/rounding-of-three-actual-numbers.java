578b6f680e48de7c07672cff68a970bbe4f542bb
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-18 15:30:07 +0900

    Title: 세 실수의 반올림, Time: 162ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240718/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+        double c = Double.parseDouble(br.readLine());
+
+        System.out.printf("%.3f\n", a);
+        System.out.printf("%.3f\n", b);
+        System.out.printf("%.3f\n", c);
+    }
+}
