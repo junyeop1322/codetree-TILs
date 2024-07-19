df15ca17d4fb233afe88f9194dc041f2f1db00f3
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-20 00:00:30 +0900

    Title: 4가지 관계연산자, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240720/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a >= b) {
+            sb.append("1\n");
+        } else {
+            sb.append("0\n");
+        }
+
+        if (a > b) {
+            sb.append("1\n");
+        } else {
+            sb.append("0\n");
+        }
+
+        if (a <= b) {
+            sb.append("1\n");
+        } else {
+            sb.append("0\n");
+        }
+
+        if (a < b) {
+            sb.append("1\n");
+        } else {
+            sb.append("0\n");
+        }
+
+        System.out.println(sb);
+    }
+}
