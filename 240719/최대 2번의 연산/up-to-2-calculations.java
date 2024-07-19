a35f60fecf9fd35647df27782cb7f4ee1867c626
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-19 23:08:25 +0900

    Title: 최대 2번의 연산, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/최대 2번의 연산/up-to-2-calculations.java
+++ 240719/최대 2번의 연산/up-to-2-calculations.java
@@ -9,10 +9,10 @@ public class Main {
 
         if (a % 2 == 0) {
             a /= 2;
+        }
 
-            if (a % 2 == 1) {
-                a = (a+1) / 2;
-            }
+        if (a % 2 == 1) {
+            a = (a+1) / 2;
         }
 
         System.out.println(a);
