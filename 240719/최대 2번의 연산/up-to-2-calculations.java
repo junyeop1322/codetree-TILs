3040ad85e6504ba1572101ab2375fb42b6854011
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-19 23:07:53 +0900

    Title: 최대 2번의 연산, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240719/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 2 == 0) {
+            a /= 2;
+
+            if (a % 2 == 1) {
+                a = (a+1) / 2;
+            }
+        }
+
+        System.out.println(a);
+
+    }
+}
