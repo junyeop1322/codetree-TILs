22a9ef7b91f477dcad21c1a2f56fece2dd68d81f
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 19:03:11 +0900

    Title: 완전수 판별기, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println("P");
+        } else {
+            System.out.println("N");
+        }
+    }
+}
