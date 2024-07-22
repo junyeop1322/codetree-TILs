b6ff101ba25cbcb37f4e21296ea79d75e46868ad
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 14:25:27 +0900

    Title: 감소시키며 출력하기, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 17; i >= 5; i--) {
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
