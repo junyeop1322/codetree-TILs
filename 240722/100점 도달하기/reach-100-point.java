34a0c0c92596bffab2cc3756f8fd7077ed40ebff
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 16:51:16 +0900

    Title: 100점 도달하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240722/100점 도달하기/reach-100-point.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i <= 100; i++) {
+            String str;
+
+            if (i >= 90) {
+                str = "A";
+            } else if (i >= 80) {
+                str = "B";
+            } else if (i >= 70) {
+                str = "C";
+            } else if (i >= 60) {
+                str = "D";
+            } else {
+                str = "E";
+            }
+
+            sb.append(str + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
