a1d8cd5e959452b7e2a40d0c588416c91385eb2c
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 18:18:30 +0900

    Title: a부터 b까지의 합, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
