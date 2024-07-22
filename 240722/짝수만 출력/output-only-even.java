28a9b1efb373e857fad447f2dba4122de25b02fa
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 15:14:43 +0900

    Title: 짝수만 출력, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/짝수만 출력/output-only-even.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        while(true) {
+            if (a > b) {
+                break;
+            }
+
+            sb.append(a + " ");
+            a += 2;
+        }
+
+        System.out.println(sb);
+    }
+}
