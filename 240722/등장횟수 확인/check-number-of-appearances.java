406a0e4e8a0e9ac44c32432ea20d2527990cdafd
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 17:58:16 +0900

    Title: 등장횟수 확인, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num % 2 == 0) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
