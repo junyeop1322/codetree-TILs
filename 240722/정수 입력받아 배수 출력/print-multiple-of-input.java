dc687530e40c227be4421ead070fc71026cdf2f9
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 14:20:32 +0900

    Title: 정수 입력받아 배수 출력, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240722/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++) {
+            sb.append(i*7 + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
