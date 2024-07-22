1abe261ab626f7c1d6cb9c5ab03a5d25c60f82f2
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 13:51:42 +0900

    Title: 차례로 출력, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/차례로 출력/print-in-order.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 5; i <= 17; i++) {
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
