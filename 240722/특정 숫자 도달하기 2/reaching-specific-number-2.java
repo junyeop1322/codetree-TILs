8a0020e6cdf0590964091ccc6293c81925274385
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 14:35:11 +0900

    Title: 특정 숫자 도달하기 2, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n; i >= 1; i--) {
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
