e304bb40df338f1123ddf64dd205c76c860d976b
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-22 19:58:21 +0900

    Title: 1부터의 합, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+
+            if (sum >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
