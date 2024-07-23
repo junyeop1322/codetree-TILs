db1cd472429b9db21a145b0c5238ebbe23a59b95
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 15:22:03 +0900

    Title: 직사각형 별표 출력하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240723/직사각형 별표 출력하기/print-stars-in-rectangle.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sb.append("* ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
