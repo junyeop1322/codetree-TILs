0eae20dd806ae6f5062bd3f1baa633b548cda7e5
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 15:15:29 +0900

    Title: 소수 판별기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240723/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String str = "P";
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                str = "C";
+                break;
+            }
+        }
+        
+        System.out.println(str);
+    }
+}
