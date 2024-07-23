ac024fb56c78e7b51f6e982aa71ab32395e985cf
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 15:05:51 +0900

    Title: 공약수의 유무, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (1920 % i == 0 || 2880 % i == 0) {
+                ans = 1;
+                break;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
