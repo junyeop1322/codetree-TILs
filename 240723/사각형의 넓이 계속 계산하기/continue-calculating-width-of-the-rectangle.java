a72509445d1d8221dad3ed4ee08ba8dad6e99d26
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 13:58:08 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240723/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            sb.append(a*b + "\n");
+
+            if (s.equals("C")) {
+                break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
