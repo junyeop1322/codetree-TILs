54e85555068a715f22546341853a2c15a7663eac
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 19:30:20 +0900

    Title: 별표 출력하기 13, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240723/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        String[] ans = new String[n];
+
+        for (int i = 1; i <= n; i++) {
+            String str = "";
+
+            if (i % 2 == 1) {
+                for (int j = 0; j < n - (i+1)/2 + 1; j++) {
+                    str += "* ";
+                }
+            } else {
+                for (int j = 0; j < i / 2; j++) {
+                    str += "* ";
+                }
+
+            }
+
+            ans[i-1] = str;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            sb.append(ans[i] + "\n");
+        }
+        for (int i = n-1; i >= 0; i--) {
+            sb.append(ans[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
