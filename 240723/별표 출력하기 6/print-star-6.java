865c494e69b5899dc6c70a39f5081a778b7045b8
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-23 18:06:50 +0900

    Title: 별표 출력하기 6, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240723/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= (n * 2) - 1; i++) {
+            if (i <= n) {
+                for (int j = 0; j < i-1; j++) {
+                    sb.append("  ");
+                }
+                for (int j = 0; j < (n + 1 - i) * 2 - 1; j++) {
+                    sb.append("* ");
+                }
+            } else {
+                for (int j = 0; j < n - (i - n) -1; j++) {
+                    sb.append("  ");
+                }
+                for (int j = 0; j < (i - n + 1) * 2 - 1; j++) {
+                    sb.append("* ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
