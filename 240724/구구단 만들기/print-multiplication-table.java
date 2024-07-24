9b76853ec39d3c4ae964bdb339531e30f43223a2
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 15:43:45 +0900

    Title: 구구단 만들기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240724/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = b; j >= a; j -= 2) {
+                sb.append(j + " * " + i + " = " + i*j + " / ");
+            }
+            sb.delete(sb.length() - 2, sb.length());
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
