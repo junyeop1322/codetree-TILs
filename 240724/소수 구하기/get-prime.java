d7ab3955670043bd40bf459d6d740c6906d4a07f
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 19:02:21 +0900

    Title: 소수 구하기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240724/소수 구하기/get-prime.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 2; i <= n; i++) {
+            if (prime(i)) {
+                sb.append(i + " ");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static boolean prime(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
