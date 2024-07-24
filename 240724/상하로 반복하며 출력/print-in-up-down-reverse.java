ce7f3c7d11fa886e038e45c705069dfe0072ce4b
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 15:08:21 +0900

    Title: 상하로 반복하며 출력, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240724/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int[][] arr = new int[n+1][n+1];
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                int cnt = 1;
+                for (int j = 1; j <= n; j++) {
+                    arr[j][i] = cnt;
+                    cnt++;
+                }
+            } else {
+                int cnt = n;
+                for (int j = 1; j <= n; j++) {
+                    arr[j][i] = cnt;
+                    cnt--;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
