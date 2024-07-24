c14ea927ee985e5d2c9369a951820abbeed8577e
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 18:31:49 +0900

    Title: 알파벳 출력하기 2, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240724/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        char ch = 'A';
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i-1; j++) {
+                sb.append("  ");
+            }
+            for (int j = 0; j < n-i+1; j++) {
+                sb.append(ch + " ");
+                ch++;
+                if (ch > 'Z') {
+                    ch = 'A';
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
