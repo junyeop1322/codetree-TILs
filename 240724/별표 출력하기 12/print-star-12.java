5489bd36a0b6c673b726b5c19757e92ff52b14d0
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 12:44:36 +0900

    Title: 별표 출력하기 12, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240724/별표 출력하기 12/print-star-12.java
+++ 240724/별표 출력하기 12/print-star-12.java
@@ -6,13 +6,13 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
+        int[][] arr = new int[n+1][n+1];
 
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                arr[0][i] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                arr[1][i] = 1;
             } else {
-                for (int j = 0; j < i; j++) {
+                for (int j = 1; j <= i; j++) {
                     arr[j][i] = 1;
                 }
             }
@@ -20,8 +20,8 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (arr[i][j] == 1) {
                     sb.append("* ");
                 } else {
