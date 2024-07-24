5115cd5fbdafe2f70b397c9831866ec956400865
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-24 19:02:49 +0900

    Title: 소수 구하기, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 240724/소수 구하기/get-prime.java
+++ 240724/소수 구하기/get-prime.java
@@ -16,7 +16,7 @@ public class Main {
     }
 
     static boolean prime(int n) {
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
