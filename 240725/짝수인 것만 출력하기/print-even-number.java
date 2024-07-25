39806103808ae951fc8110f1f04a996c2c0c0e10
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-25 12:28:43 +0900

    Title: 짝수인 것만 출력하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240725/짝수인 것만 출력하기/print-even-number.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num % 2 == 0) {
+                sb.append(num + " ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
