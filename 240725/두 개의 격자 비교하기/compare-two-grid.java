239b717ff97fe4f4be804dc20e72d5185eca923d
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-25 18:49:23 +0900

    Title: 두 개의 격자 비교하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240725/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == Integer.parseInt(st.nextToken())) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(1 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
