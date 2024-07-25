d2974be99cdcfe7819a28e19ac93cac6b9386dec
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-25 13:10:13 +0900

    Title: 주사위 놀이, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240725/주사위 놀이/play-with-dice.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[7];
+
+        for (int i = 0; i < 10; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            arr[n]++;
+        }
+
+        for (int i = 1; i <= 6; i++) {
+            sb.append(i + " - " + arr[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
