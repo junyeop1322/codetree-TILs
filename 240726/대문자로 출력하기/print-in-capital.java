1776156f33893c70379b888d77247551772bd1ed
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-26 22:23:01 +0900

    Title: 대문자로 출력하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) >= 'a' && str.charAt(i) <= 'z') || (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')) {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        str = sb.toString();
+
+        System.out.println(str.toUpperCase());
+    }
+}
