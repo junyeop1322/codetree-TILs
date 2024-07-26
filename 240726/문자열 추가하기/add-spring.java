af7173c39921c904ce6562c018bda50e32e21f8c
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-26 15:22:34 +0900

    Title: 문자열 추가하기, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str + "Hello");
+    }
+}
