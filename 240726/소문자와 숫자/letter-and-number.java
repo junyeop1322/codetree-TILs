b52647a21c7b9bdd2946f12695f07424ae4d6d08
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-26 22:28:57 +0900

    Title: 소문자와 숫자, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) >= 'a' && str.charAt(i) <= 'z') || (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') || (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        str = sb.toString();
+
+        System.out.println(str.toLowerCase());
+
+
+    }
+}
