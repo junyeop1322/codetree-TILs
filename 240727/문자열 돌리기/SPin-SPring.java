f79b8ae82b56832bd8d95f56849f2e389d97a45e
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-27 22:30:03 +0900

    Title: 문자열 돌리기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(str);
+
+        for (int i = 0; i < str.length(); i++) {
+            str = str.substring(str.length()-1, str.length()) + str.substring(0, str.length()-1);
+            System.out.println(str);
+        }
+    }
+}
