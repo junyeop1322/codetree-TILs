077ff44ac9c8c06d80f368d7e7e60df23d6cd72c
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-27 23:19:20 +0900

    Title: 미는 횟수, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240727/미는 횟수/number-of-pushes.java
+++ 240727/미는 횟수/number-of-pushes.java
@@ -17,7 +17,7 @@ public class Main {
                 break;
             }
 
-            str2 = str2.substring(str2.length()-1, str2.length()) + str2.substring(0, str2.length()-1);
+            str1 = str1.substring(str1.length()-1, str1.length()) + str1.substring(0, str1.length()-1);
 
             if (str2.equals(str1)) {
                 System.out.println(cnt);
