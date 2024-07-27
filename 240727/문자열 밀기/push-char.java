2fac6c9b77a849e5cdfd2296a749e56abdb3384f
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-27 22:25:10 +0900

    Title: 문자열 밀기, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/문자열 밀기/push-char.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        str = str.substring(1, str.length()) + str.substring(0, 1);
+
+        System.out.println(str);
+    }
+}
