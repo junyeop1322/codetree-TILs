2b8ece2774e4def2b7d35aa946b13dce13177758
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-27 22:56:43 +0900

    Title: 아스키코드 변환, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println(((int)st.nextToken().charAt(0)) + " " + ((char)Integer.parseInt(st.nextToken())));
+    }
+}
