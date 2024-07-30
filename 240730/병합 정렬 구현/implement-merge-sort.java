fc5f64fb130984778dfecefaeed277db6f4b3c98
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-30 11:41:31 +0900

    Title: 병합 정렬 구현, Time: 901ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240730/병합 정렬 구현/implement-merge-sort.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
