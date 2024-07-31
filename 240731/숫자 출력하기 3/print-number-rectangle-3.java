c938dff047fbeddb7f0e0feb16106739add830fc
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-31 15:45:26 +0900

    Title: 숫자 출력하기 3, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/숫자 출력하기 3/print-number-rectangle-3.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+        int num = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
