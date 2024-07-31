e70be5e73e2de7f9371a0430faa7ddb9bd7176a1
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-07-31 18:35:17 +0900

    Title: 정수의 최솟값, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240731/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(findMin(a, b, c));
+    }
+
+    static int findMin(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+}
