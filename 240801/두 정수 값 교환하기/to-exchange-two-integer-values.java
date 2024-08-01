45629e8c67b94b6f2adf4837c9db1ddf1902b7f8
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-01 12:17:47 +0900

    Title: 두 정수 값 교환하기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240801/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        swap(n, m);
+    }
+
+    static void swap(int n, int m) {
+        System.out.println(m + " " + n);
+    }
+}
