63d2106dbfa7dea5ceade7c6f92c59f304252263
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-01 17:46:16 +0900

    Title: 단어 정렬, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240801/단어 정렬/sorting-words.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
