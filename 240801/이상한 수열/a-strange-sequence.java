79bbf8f27cc8d10f237f92e79b1bc11d6eed72ce
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-01 16:04:58 +0900

    Title: 이상한 수열, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240801/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.println(findSeq(n));
+    }
+
+    static int findSeq(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        return findSeq(n-1) + (n/3);
+    }
+}
