f8c2542983c15a647e4f0d3d0a7340419f7f33af
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-02 19:51:12 +0900

    Title: DateTime to DateTime, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240802/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a < 11) {
+            System.out.println(-1);
+            return;
+        } else {
+            if (b < 11) {
+                System.out.println(-1);
+                return;
+            } else {
+                if (c < 11) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        int d = 0;
+        int h = 0;
+        int m = 0;
+
+        if (b >= 11) {
+            d = a - 11;
+        } else {
+            d = a - 12;
+        }
+
+        if (c >= 11) {
+            h = b - 11;
+            m = c - 11;
+        } else {
+            h = b - 12;
+            m = 60 - (11 - c);
+        }
+
+        System.out.println(m + 60*h + 24*60*d);
+    }
+}
