1fee3ca9b2e510dece2280f65fa9c3d5756a9ce5
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-04 22:58:59 +0900

    Title: 진법 변환 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240804/진법 변환 3/base-conversion-3.java
+++ 240804/진법 변환 3/base-conversion-3.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String str = st.nextToken();
 
         System.out.println(Integer.toString(Integer.parseInt(str, 8), 2));
     }
