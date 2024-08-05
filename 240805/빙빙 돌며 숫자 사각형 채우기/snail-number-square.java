6883bb63c80408d3c1b086408c8fac5f2387a533
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-05 16:15:38 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240805/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 240805/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -19,6 +19,11 @@ public class Main {
         int x = 0;
         int y = 0;
 
+        if (n == 1 && m == 1) {
+            System.out.println(1);
+            return;
+        }
+
         for (int i = 1; i <= n*m; i++) {
             arr[x][y] = i;
 
