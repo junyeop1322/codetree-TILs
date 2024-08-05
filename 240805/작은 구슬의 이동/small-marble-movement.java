26d60d9a31d94277c16cea5ab311ebe19cd9e03a
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-05 15:36:01 +0900

    Title: 작은 구슬의 이동, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240805/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        String order = st.nextToken();
+
+        for (int i = 0; i < t; i++) {
+            if (order.equals("L")) {
+                if (y == 1) {
+                    order = "R";
+                } else {
+                    y--;
+                }
+            } else if (order.equals("R")) {
+                if (y == n) {
+                    order = "L";
+                } else {
+                    y++;
+                }
+            } else if (order.equals("U")) {
+                if (x == n) {
+                    order = "D";
+                } else {
+                    x++;
+                }
+            } else if (order.equals("D")) {
+                if (x == 1) {
+                    order = "U";
+                } else {
+                    x--;
+                }
+            }
+        }
+
+        System.out.println(x + " " + y);
+
+    }
+}
