c882de625b4f99beafedad3eab4a4c114fe30069
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-05 12:26:15 +0900

    Title: 원의 넓이를 출력하는 함수, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240805/원의 넓이를 출력하는 함수/function-that-outputs-area-of-circle.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.printf("%.2f", n*n*3.14);
+    }
+}
