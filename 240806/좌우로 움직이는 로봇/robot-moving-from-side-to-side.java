ea31a915fa4e2b4a6dfacd7a637240b8eb5e82b3
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-06 11:57:56 +0900

    Title: 좌우로 움직이는 로봇, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+class Ro {
+    int t;
+    String v;
+
+    public Ro(int t, String v) {
+        this.t = t;
+        this.v = v;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Ro[] roA = new Ro[n];
+        Ro[] roB = new Ro[m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            roA[i] = new Ro(Integer.parseInt(st.nextToken()), st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            roB[i] = new Ro(Integer.parseInt(st.nextToken()), st.nextToken());
+        }
+
+        int cnt = 1;
+
+        int idxA = 0;
+        int idxB = 0;
+
+        int cntA = 0;
+        int cntB = 0;
+
+        int a = 0;
+        int b = 0;
+
+        while (true) {
+            int at = roA[idxA].t;
+            String av = roA[idxA].v;
+
+            int bt = roB[idxB].t;
+            String bv = roB[idxB].v;
+
+            boolean check = false;
+
+            if (a != b) {
+                check = true;
+            }
+            
+            if (av.equals("L")) {
+                a--;
+                cntA++;
+            } else {
+                a++;
+                cntA++;
+            }
+
+            if (bv.equals("L")) {
+                b--;
+                cntB++;
+            } else {
+                b++;
+                cntB++;
+            }
+
+            // 처음에는 다른 위치에 있다가 같은 위치가 되면 cnt++
+            if (check && a == b) {
+                cnt++;
+            }
+
+            if (cntA == at) {
+                idxA++;
+                cntA = 0;
+            }
+
+            if (cntB == bt) {
+                idxB++;
+                cntB = 0;
+            }
+
+            if (idxA == n || idxB == m) {
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
