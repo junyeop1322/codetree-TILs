43eb9b004ee76aebaf8511d9f4233f6efaf7ae38
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-06 19:49:58 +0900

    Title: 가장 가까운 소수, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/가장 가까운 소수/closest-prime.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(2);
+            return;
+        } else if (n == 2) {
+            System.out.println(3);
+            return;
+        }
+
+        int num = n-1;
+
+        int ans1 = 0;
+        int ans2 = 0;
+
+        while (true) {
+            boolean check = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) {
+                ans1 = num;
+                break;
+            }
+            num--;
+        }
+
+        num = n+1;
+        
+        while (true) {
+            boolean check = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) {
+                ans2 = num;
+                break;
+            }
+            num++;
+
+            if (n - ans1 == ans2 - n) {
+                System.out.println(ans1 + " " + ans2);
+            } else {
+                if (n - ans1 < ans2 - n) {
+                    System.out.println(ans1);
+                } else {
+                    System.out.println(ans2);
+                }
+            }    
+            
+        }
+    }
+}
