4015eecf11d6816ff1fdaf313e33ce90783df67d
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-06 16:26:09 +0900

    Title: 최고의 13위치, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n-3; j++) {
+                int sum = 0;
+                sum += arr[i][j];
+                sum += arr[i][j+1];
+                sum += arr[i][j+2];
+
+                max = Math.max(max, sum);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
