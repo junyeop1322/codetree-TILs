145465183452fb0b6bf4e63ec0002fcb137db77d
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-06 16:20:27 +0900

    Title: 일렬로 서있는 소 2, Time: 123ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i+1; j < n-1; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if (arr[i] <= arr[j] && arr[j] <= arr[k]) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+}
