9f29ff8a367751bfa82d1d89db339047efc8cfd1
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-08 13:46:04 +0900

    Title: 구간 중 최대 합, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240808/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= n-k; i++) {
+            int sum = 0;
+            for (int j = i; j < i+3; j++) {
+                sum += arr[j];
+            }
+            ans = Math.max(ans, sum);
+        }
+
+        System.out.println(ans);
+    }
+}
