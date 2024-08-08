45eac06b413b19ca0ae75c426bf4f829a5255522
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-08 13:07:06 +0900

    Title: 최대로 겹치는 구간, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240808/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,35 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
 
-        int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[201];
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken()) + 1;
-            int y = Integer.parseInt(st.nextToken()) - 1;
+    public static int[] checked = new int[MAX_R + 1];
 
-            if (x <= y) {
-                for (int j = x+100; j <= y+100; j++) {
-                    arr[j]++;
-                }
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
         }
 
+        // 구간을 칠해줍니다.
+        // 구간 단위로 진행하는 문제이므로
+        // x2[i]에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = x1[i]; j < x2[i]; j++)
+                checked[j]++;
+        
+        // 최댓값을 구합니다.
         int max = 0;
-        for (int i = 0; i < 201; i++) {
-            // System.out.print(arr[i] + " ");
-            max = Math.max(max, arr[i]);
-        }
-        // System.out.println();
-
-        System.out.println(max);
+        for(int i = 0; i <= MAX_R; i++)
+            if(checked[i] > max)
+                max = checked[i];
+        
+        // 출력
+        System.out.print(max);
     }
 }
