42cb99d745aaed722420a6c04b4faf608a62bc2a
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-09 15:45:02 +0900

    Title: 지뢰 찾기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240809/지뢰 찾기/minesweeper.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dy = {-1, 0, 1, 1, -1, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        char[][] map = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        char[][] check = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                check[i][j] = str.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (check[i][j] == 'x') {
+                    int cnt = 0;
+                    for (int k = 0; k < 8; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+
+                        if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                            if (map[nx][ny] == '*') {
+                                cnt++;
+                            }
+                        }
+                    }
+                    sb.append(cnt);
+                } else {
+                    sb.append(".");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
