c1055415b717398330f7df31da0f268746e08703
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-09 14:33:05 +0900

    Title: 겹치지 않는 선분 2, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240809/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            x1[i] = Integer.parseInt(st.nextToken());
+            x2[i] =Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n-1; i++) {
+
+            boolean overlap = false;
+
+            for(int j = i+1; j < n; j++) {
+                if((x1[i] <= x1[j] && x2[i] >= x2[j]) || (x1[i] >= x1[j] && x2[i] <= x2[j])) {
+                    overlap = true;
+                    break;
+                }
+            }
+
+            if(overlap == false)
+                ans++;
+        }
+
+        System.out.print(ans);
+
+    }
+}
