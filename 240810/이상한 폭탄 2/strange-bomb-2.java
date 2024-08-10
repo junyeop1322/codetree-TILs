c84d7c5b4d84a4cf114a3361ee4c6ab1d0f7fae8
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-10 11:41:14 +0900

    Title: 이상한 폭탄 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240810/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = -1;
+        int ans = -1;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = fun(arr, arr[i], k);
+
+            if (cnt != 0) {
+                if (cnt > max) {
+                    max = cnt;
+                    ans = arr[i];
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static int fun(int[] arr, int num, int k) {
+        int cnt = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= num-k && arr[i] <= num+k) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
