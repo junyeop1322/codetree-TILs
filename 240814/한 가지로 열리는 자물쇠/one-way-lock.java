c6012170acaac5f5fa82c4f2dae6c2d4d9edf460
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-14 13:33:22 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240814/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (Math.abs(a - i) <= 2 || Math.abs(b - j) <= 2 || Math.abs(c - k) <= 2) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
