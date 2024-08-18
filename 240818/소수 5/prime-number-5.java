33dcb92e54d71f3da0079117ebf23956aa0d58a3
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-18 18:07:36 +0900

    Title: 소수 5, Time: 277ms, Memory: 19MB, Status: Passed - CodeTree

--- 240818/소수 5/prime-number-5.java
+++ 240818/소수 5/prime-number-5.java
@@ -10,22 +10,29 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        int ans = 0;
-        for (int i = a; i <= b; i++) {
-            ans += isPrime(i);
+        boolean[] isPrime = new boolean[b + 1];
+        for (int i = 2; i <= b; i++) {
+            isPrime[i] = true;
         }
+
+        for (int i = 2; i * i <= b; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= b; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (isPrime[i]) {
+                ans++;
+            }
+        }
+
 
         System.out.println(ans);
     }
 
-    static int isPrime(int num) {
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return 0;
-            }
-        }
-
-        return 1;
-    }
 }
