e86e9d2ada5081f260356edb11301a3cb3f5d84e
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-19 12:05:40 +0900

    Title: 양수 직사각형의 최대 크기, Time: 76ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240819/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[][] arr;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        ans = -1;
+
+        func(1, 1);
+
+        System.out.println(ans);
+    }
+
+    static void func(int x, int y) {
+        if (x == n + 1 || y == m + 1) {
+            return;
+        }
+
+        boolean check = false;
+
+        for (int i = 0; i <= n - x; i++) {
+            for (int j = 0; j <= m - y; j++) {
+                check = true;
+                for (int k = i; k < i + x; k++) {
+                    for (int l = j; l < j + y; l++) {
+                        if (arr[k][l] < 0) {
+                            check = false;
+                        }
+                    }
+                }
+
+                if (check) {
+                    ans = Math.max(ans, x * y);
+                }
+            }
+
+        }
+
+
+        func(x+1, y);
+        func(x, y+1);
+    }
+}
