a1f96837078523c42d8f42a01872d4a914ba1558
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-21 13:41:51 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 2305ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240821/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[][] arr;
+    static boolean[][] visited;
+    static Queue<int[]> q = new LinkedList<>();
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        bfs();
+
+
+    }
+
+    static void bfs() {
+        q.add(new int[] {0, 0});
+
+        while(!q.isEmpty()) {
+            int[] a = q.poll();
+            int x = a[0];
+            int y = a[1];
+
+            visited[x][y] = true;
+
+            if (x == n-1 && y == m-1) {
+                System.out.println(1);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (arr[nx][ny] == 1 && !visited[nx][ny]) {
+                        q.add(new int[] {nx, ny});
+                    }
+                }
+            }
+
+
+        }
+
+        System.out.println(0);
+    }
+}
