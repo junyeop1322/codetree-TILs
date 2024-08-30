bdb4f58bb5c6c15ecda0285c4caddf90da4a23b6
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-08-30 23:46:27 +0900

    Title: 더하기 사이클, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240830/더하기 사이클/additional-cycles.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        String str = Integer.toString(n);
+        int cnt = 1;
+
+        int num = n;
+
+        while (true) {
+            int number = 0;
+            
+            number += (num / 10) + (num % 10);
+
+            String strA = Integer.toString(number);
+            int a = strA.charAt(strA.length() - 1) - '0';
+
+            String strB = Integer.toString(num);
+            int b = strB.charAt(strB.length() - 1) - '0';
+            
+            num = 10*a + b;
+            cnt++;
+
+            if (num == n) {
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
