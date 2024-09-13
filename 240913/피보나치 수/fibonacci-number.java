45d974d486d95d25f30d1ae9995b03a82e7b981c
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-09-13 23:35:22 +0900

    Title: 피보나치 수, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240913/피보나치 수/fibonacci-number.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long[] memo;
+    public static long fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        else if (memo[n] != 0)
+            return memo[n];
+        else
+            return memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+ 
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        memo = new long[n+1];
+        System.out.println(fibonacci(n));
+    }
+}
