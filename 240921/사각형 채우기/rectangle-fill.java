f49b91342d75d91889d8d214904e0a8a371b30b3
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-09-21 21:09:06 +0900

    Title: 사각형 채우기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240921/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
