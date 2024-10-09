71c81d2d20d33d45a0e5b57c7be3a676a426b11d
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-09 11:23:27 +0900

    Title: 아스키코드 순, Time: 144ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241009/아스키코드 순/ascii-in-order.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            str[i] = br.readLine();
+        }
+
+        Arrays.sort(str);
+
+        System.out.println(str[0]);
+    }
+}
