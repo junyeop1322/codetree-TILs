947e7843d7f5df1eb4d18b907043d391bd26e223
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-10 12:25:25 +0900

    Title: 문자열의 개수 2, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241010/문자열의 개수 2/number-of-spring-2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = 0;
+        while (true) {
+            String str = br.readLine();
+
+            if (str.equals("0")) {
+                break;
+            }
+
+            cnt++;
+
+            if (cnt % 2 == 0) {
+                sb.append(str + "\n");
+            }
+        }
+
+        System.out.println(cnt);
+        System.out.println(sb);
+    }
+}
