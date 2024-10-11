7056b1c234d986d3b7a9cdfa2791390bb4a38f34
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-11 13:48:34 +0900

    Title: 문자열의 길이 순, Time: 226ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241011/문자열의 길이 순/string-length-order.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (String s1, String s2) -> s1.length() - s2.length());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
