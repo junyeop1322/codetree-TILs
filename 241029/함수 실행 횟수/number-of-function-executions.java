4d7e799b42466a0d3057842345ae5a67042c40df
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-29 14:07:55 +0900

    Title: 함수 실행 횟수, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 241029/함수 실행 횟수/number-of-function-executions.java
+++ 241029/함수 실행 횟수/number-of-function-executions.java
@@ -12,9 +12,9 @@ public class Main {
         long[] dp = new long[n+1];
 
         dp[0] = 1;
-        dp[1] = 2;
+        dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+            dp[i] = ((dp[i-1] + dp[i-2]) + 1) % 1000000007;
         }
 
         System.out.println(dp[n]);
