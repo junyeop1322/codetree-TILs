3ca3424c69c47bf87fb61ae22746c99569768d04
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-29 14:04:21 +0900

    Title: 함수 실행 횟수, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/함수 실행 횟수/number-of-function-executions.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        long[] dp = new long[n+1];
+
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
