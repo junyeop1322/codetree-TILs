97ae1ce14889da66c12df8795acd5ef80e7a12c0
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-10-31 12:09:04 +0900

    Title: 단어 사전, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241031/단어 사전/word-dict.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] str = br.readLine().split(" ");
+
+        Arrays.sort(str);
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length; i++) {
+            set.add(str[i]);
+        }
+
+        Iterator iter = set.iterator();
+        while(iter.hasNext()) {
+            System.out.print(iter.next() + " ");
+        }
+    }
+}
