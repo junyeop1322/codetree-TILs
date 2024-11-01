4364c12237d83d6d9b3d60ec8e5b5561c5299325
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-11-02 00:31:14 +0900

    Title: 소수와 합성수, Time: 3089ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 241102/소수와 합성수/prime-composition.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println("one");
+        } else {
+            if (isPrime(n)) {
+                System.out.println("prime");
+            } else {
+                System.out.println("composition");
+            }
+        }
+    }
+
+    static boolean isPrime(int n) {
+        for (int i = 2; i*i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
