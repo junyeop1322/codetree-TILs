c0230b1c0346fdc70b381238a16144f045957497
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-11-02 00:31:57 +0900

    Title: 소수와 합성수, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

--- 241102/소수와 합성수/prime-composition.java
+++ 241102/소수와 합성수/prime-composition.java
@@ -19,7 +19,7 @@ public class Main {
     }
 
     static boolean isPrime(int n) {
-        for (int i = 2; i*i <= n; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
