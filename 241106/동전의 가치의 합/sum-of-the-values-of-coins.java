d28cf9cff11a9806e3d745a83fac8fc7f9db03bc
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-11-06 11:48:12 +0900

    Title: 동전의 가치의 합, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241106/동전의 가치의 합/sum-of-the-values-of-coins.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[100001];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 2;
+        dp[5] = 1;
+        dp[6] = 2;
+        dp[7] = 1;
+
+        for (int i = 8; i <= 100000; i++) {
+            dp[i] = Math.min(dp[i-1], Math.min(dp[i-2], Math.min(dp[i-5], dp[i-7]))) + 1;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
