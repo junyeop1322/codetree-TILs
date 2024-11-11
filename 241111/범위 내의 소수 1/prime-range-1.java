bf1506dce468c32f0aa553d3227fdfaa34dbc191
YEOP <88226521+junyeop1322@users.noreply.github.com>
2024-11-11 14:06:40 +0900

    Title: 범위 내의 소수 1, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241111/범위 내의 소수 1/prime-range-1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                min = Math.min(min, i);
+            }
+        }
+
+        System.out.println(sum + " " + min);
+    }
+
+    static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % 2 == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
